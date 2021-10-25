c1365a7a7c96e995e1c0e75822be741d90cb2e6f
belnek <belnek.it.os@gmail.com>
2021-10-25 18:49:49 +0400

    12

---
+++ app/src/main/java/com/belnek/roundmath/LessonCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.belnek.roundmath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LessonCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lessoncategory);
+    }
+}
