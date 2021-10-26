5f11a5220e686e28cd5fe234c3889bb58191bf12
belnek <belnek.it.os@gmail.com>
2021-10-26 19:10:55 +0400

    12

--- app/src/main/java/com/belnek/roundmath/MainActivity.java
+++ app/src/main/java/com/belnek/roundmath/MainActivity.java
@@ -3,12 +3,29 @@ package com.belnek.roundmath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonless = (Button) findViewById(R.id.button_startless);
+        Button buttontasks = (Button)findViewById(R.id.button_startTasks);
+
+        buttonless.setOnClickListener(this);
+        buttontasks.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_startless:
+                break;
+
+        }
     }
 }
